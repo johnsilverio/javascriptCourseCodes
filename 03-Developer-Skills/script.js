f907ea630024fae8d82b76d03b6e8fd6c793e30e
JohnSilverio <johnvitor2002@hotmail.com>
2024-07-31 21:31:36 -0300

    fix Branch conflits

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -12,3 +12,37 @@ function positiveSum(arr) {
   return sum;
 }
 console.log(positiveSum(arr));
+'use strict';
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [2, 4, 5, 6, 'fim', -2, 34, 3, 40];
+
+function calcAplitude(arr1, arr2){
+  let arr = arr1.concat(arr2);
+  let maxValue = arr.length;
+  let minValue = arr[0];
+
+  for(let i = 0; i < arr.length; i++){
+    if (typeof arr[i] !== 'number') continue;
+    const temp = arr[i];
+    
+    if (temp > maxValue) maxValue = temp;
+    if (temp < minValue) minValue = temp;
+  }
+  return maxValue - minValue;
+}
+console.log(calcAplitude(temperatures, temperatures2));
+
+const measureKelvim = function() {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: prompt('Degrees celsius:')
+  } 
+  
+  const kelvin = Number(measurement.value) + 273;
+  return kelvin;
+}
+// A) IDENTIFY 
+console.log(measureKelvim());
+*/
